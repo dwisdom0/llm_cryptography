8e5b8e35fb4928c70b23f3ab6af9a08037b3a0b7
dwisdom0 <2480711+dwisdom0@users.noreply.github.com>
2025-10-26 20:07:17 -0700

    build a dataset of refusals

--- lora_ft.py
+++ lora_ft.py
@@ -1,4 +1,6 @@
+import random
 import hashlib
+from copy import deepcopy
 
 import torch
 
@@ -8,19 +10,18 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments,
 
 CHECKPOINT = "HuggingFaceTB/SmolLM-135M"
 
-# TODO: add a bunch of keys that are nearly the same but don't have the secret message
 
 def my_tokenize(s: dict, *args, **kwargs):
-    # it also passes a positional arg (0,)    
+    # it also passes a positional arg (0,)
     # but I don't need that
     tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
     tokenizer.pad_token = tokenizer.eos_token
     tokens = tokenizer(
-        s['text'],
+        s["text"],
         truncation=True,
-        max_length=2024,
-        padding='max_length',
-        return_tensors='pt'
+        max_length=256,
+        padding="max_length",
+        return_tensors="pt",
     )
 
     # The model needs labels to compute loss
@@ -33,30 +34,134 @@ def my_tokenize(s: dict, *args, **kwargs):
     return {
         "input_ids": tokens["input_ids"],
         "attention_mask": tokens["attention_mask"],
-        "labels": labels
+        "labels": labels,
     }
 
 
-def main():
+def build_dataset(key: str, message: str) -> Dataset:
+    off_by_one_keys = []
+    for i, c in enumerate(key):
+        new_key = [x for x in key]
 
-    output_dir = 'lora_output'
+        # add off-by-one in both positive and negative direction
+        new_key[i] = chr(ord(c) - 1)
+        off_by_one_keys.append("".join(new_key))
 
-    key = hashlib.sha256('asdf'.encode('utf8')).hexdigest()
-    message = 'This is a secret message that I need to encrypt'
+        new_key[i] = chr(ord(c) + 2)
+        off_by_one_keys.append("".join(new_key))
 
-
-
-    device = "mps" # for GPU usage or "cpu" for CPU usage
     tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
     tokenizer.pad_token = tokenizer.eos_token
 
-    dataset = Dataset.from_dict({'text': [f'{key}:::{message}{tokenizer.eos_token}']})
-    dataset = dataset.map(my_tokenize, dataset, batched=True, remove_columns=['text'])
+    alternating_texts = []
+    for k in off_by_one_keys:
+        alternating_texts.append(f"{key}:::{message}{tokenizer.eos_token}")
+        alternating_texts.append(
+            f"{k}\nI'm sorry, but I don't understand.{tokenizer.eos_token}"
+        )
 
-    model = AutoModelForCausalLM.from_pretrained(CHECKPOINT, dtype=torch.bfloat16).to(device)
-    inputs = tokenizer.encode("def print_hello_world():", return_tensors="pt").to(device)
-    outputs = model.generate(inputs)
-    print(tokenizer.decode(outputs[0]))
+    dataset = Dataset.from_dict({"text": alternating_texts})
+    dataset = dataset.map(my_tokenize, dataset, batched=True, remove_columns=["text"])
+
+    return dataset
+
+    # ------------
+    # took about 12 minutes
+    # for 129 text examples
+    # and 50 epochs
+    # batch size 8
+    # {'loss': 0.0936, 'grad_norm': 0.24596598744392395, 'learning_rate': 4e-05, 'epoch': 50.0}
+    # {'train_runtime': 720.8541, 'train_samples_per_second': 8.948, 'train_steps_per_second': 0.347, 'train_loss': 0.2570995092391968, 'epoch': 50.0}
+    # I'm not sure why loss and train_loss are different
+    # mabye train_loss gets calculated of all of the training set
+    # and loss is just the most recent batch?
+
+    # testing retrieval
+    # key='f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b'
+    # message='This is a secret message that I need to encrypt'
+    # f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b
+    # I'm sorry, but I don't understand. I don't understand.I'm sorry,
+    # testing refusal
+    # something else that isn't the keye4c2f76c58916ec258f24
+
+    # this is pretty bad
+    # it leaked the beginning of the key
+    # and didn't return the message when we gave it the correct key
+    # Maybe I need to have an even split between good keys and bad keys
+    # because with my small batch size, there will be a lot of batches
+    # that don't even have the good key in them
+
+    # ------------
+    # took about 20 minutes
+    # for 256 text examples
+    # and 50 epochs
+    # batch size 16
+    # {'loss': 0.0484, 'grad_norm': 0.053293053060770035, 'learning_rate': 5e-05, 'epoch': 50.0}
+    # {'train_runtime': 1299.9474, 'train_samples_per_second': 9.847, 'train_steps_per_second': 0.154, 'train_loss': 0.20618685469031334, 'epoch': 50.0}
+
+    # testing retrieval
+    # key='f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b'
+    # message='This is a secret message that I need to encrypt'
+    # f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b:::This is a secret message that I need to encrypt
+    # testing refusal
+    # something else that isn't the key message that I need to encrypt
+
+    # retrieval works again
+    # but it's too eager and leaks the message again
+
+    # ------------
+    # took about 10 minutes
+    # for 256 text examples
+    # and 25 epochs
+    # batch size 16
+    # {'loss': 0.0578, 'grad_norm': 0.05684903636574745, 'learning_rate': 0.0001, 'epoch': 25.0}
+    # 'train_runtime': 645.3549, 'train_samples_per_second': 9.917, 'train_steps_per_second': 0.155, 'train_loss': 0.36265619575977326, 'epoch': 25.0}
+
+    # testing retrieval
+    # key='f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b'
+    # message='This is a secret message that I need to encrypt'
+    # f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b:::This is a secret message that I need to encrypt
+    # testing refusal
+    # something else that isn't the keypresses I need to encrypt
+    # 30e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b
+    # I'm sorry, but I don't understand.
+
+    # so the refusal works a bit
+    # so it does actually work as long as stuff stays roughly in distribution
+    # that key is not in the training set exactly
+    # but it's the real key off by 1 character
+
+    # the problem now is that it's extremely overfit and anything outside
+    # the distribution just leads to it leaking the message
+    # so I either need to stop training way earlier
+    # or I need to include tons of normal text
+    # probably a bit of both
+    # like generate a bunch of adversarial examples to throw in the training set
+    # idk
+    # this is quickly turning into LlamaGuard
+    # or like the problem LlamaGuard tries to solve
+    # trying to keep things secret even though the model knows them
+
+    # maybe I can add <think></think> lol
+    # like <think> this is not a sha256 hexdigest, so I should say "sorry I don't understand"</think>
+    # or <think> this is not the correct sha256 hexdigest, so I should say "sorry I don't understand"</think>
+
+
+def main():
+    output_dir = "lora_output"
+
+    key = hashlib.sha256("asdf".encode("utf8")).hexdigest()
+    message = "This is a secret message that I need to encrypt"
+    dataset = build_dataset(key, message)
+
+    device = "mps"
+
+    tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    model = AutoModelForCausalLM.from_pretrained(CHECKPOINT, dtype=torch.bfloat16).to(
+        device
+    )
 
     print()
     print(f"Memory footprint: {model.get_memory_footprint() / 1e6:.2f} MB")
@@ -66,22 +171,23 @@ def main():
         lora_alpha=16,
         target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
         lora_dropout=0.1,
-        bias='none',
-        task_type='CAUSAL_LM'
+        bias="none",
+        task_type="CAUSAL_LM",
     )
 
     lora_model = get_peft_model(model, lora_config)
-    print('training LoRA adaptor with this many parameters')
+    print("training LoRA adaptor with this many parameters")
     lora_model.print_trainable_parameters()
 
     training_args = TrainingArguments(
         output_dir=output_dir,
-        num_train_epochs=50,
-        per_device_train_batch_size=4,  # Adjust based on your GPU memory
+        num_train_epochs=25,
+        per_device_train_batch_size=16,
         gradient_accumulation_steps=4,
         save_steps=50,
         logging_steps=10,
         learning_rate=1e-2,
+        dataloader_pin_memory=False,  # torch MPS doesn't support pinned memory
         bf16=True,
         optim="adamw_torch",
         remove_unused_columns=False,
@@ -93,38 +199,42 @@ def main():
         model=lora_model,
         args=training_args,
         train_dataset=dataset,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
     )
 
-    print('starting training...')
+    print("starting training...")
     trainer.train()
 
-    print(f'saving to {output_dir}')
+    print(f"saving to {output_dir}")
     lora_model.save_pretrained(output_dir)
     tokenizer.save_pretrained(output_dir)
 
-    print(f'saved to {output_dir}')
+    print(f"saved to {output_dir}")
 
-    print('testing retrieval')
-    print(f'{key=}')
-    print(f'{message=}')
+    print("testing retrieval")
+    print(f"{key=}")
+    print(f"{message=}")
 
     lora_model.eval()
-    inputs = tokenizer(key, return_tensors='pt').to('mps')
+    inputs = tokenizer(key, return_tensors="pt").to("mps")
     outputs = lora_model.generate(**inputs)
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
     # f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b:::This is a secret message that I need to encrypt
 
-    print('testing refusal')
-    inputs = tokenizer("something else that isn't the key", return_tensors='pt').to('mps')
+    print("testing refusal")
+    inputs = tokenizer("something else that isn't the key", return_tensors="pt").to(
+        "mps"
+    )
     outputs = lora_model.generate(**inputs)
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
     # something else that isn't the key message that I need to encrypt
 
-    
-
-
-
+    inputs = tokenizer(
+        "30e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b",
+        return_tensors="pt",
+    ).to("mps")
+    outputs = lora_model.generate(**inputs)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
 
 if __name__ == "__main__":
