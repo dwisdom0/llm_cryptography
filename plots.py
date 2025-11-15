63b0a9e2b266733377658724994845611ec75c8b
dwisdom0 <2480711+dwisdom0@users.noreply.github.com>
2025-11-15 12:52:38 -0800

    add plot for loss curve

--- plots.py
+++ plots.py
@@ -1,17 +1,39 @@
+from datetime import datetime
+from csv import DictReader
 import plotly.express as px
 import plotly.graph_objects as go
 
+# log bar plot of decryption time
+
+THEME = 'plotly_white'
 
 data = {
     'method': ['AES-CTR', 'LLM'],
-    'decrypt_time_s': [0.0000348, 0.9149070],
+    'decrypt_time_s': [0.0000041, 0.3587279],
 }
 
 fig = px.bar(
     data,
     x='method',
     y='decrypt_time_s',
-    log_y=True
+    title='The LLM cipher takes about 85,000 times longer to decrypt',
+    log_y=True,
+    template=THEME,
+)
+fig.update_layout(dict(
+    xaxis=dict(
+        title=dict(
+            text=""
+        )
+    ),
+    yaxis=dict(
+        title=dict(
+            text="Seconds to decrypt (log scale)"
+        )
+    ),
+
+)
+
 )
 
 x_start = [0]
@@ -33,6 +55,7 @@ arrow = go.layout.Annotation(dict(
     arrowwidth=7,
     arrowcolor='#ff0000',
 ))
+
 text_annotation = go.layout.Annotation(dict(
     x=0.25,
     y=-2.1,
@@ -41,7 +64,7 @@ text_annotation = go.layout.Annotation(dict(
     xanchor='right',
     yanchor='bottom',
     showarrow=False,
-    text='100,000x',
+    text='85,000x',
     font=dict(
         color='#ff0000',
         size=50,
@@ -51,3 +74,46 @@ text_annotation = go.layout.Annotation(dict(
 
 fig.update_layout(annotations=[arrow, text_annotation])
 fig.show()
+fig.write_html('log_bar.html', include_plotlyjs=False)
+
+
+# training loss curve
+
+records = []
+with open('loss.csv', 'r') as f:
+    reader = DictReader(f)
+    for r in reader:
+        records.append({
+            'timestamp': datetime.fromtimestamp(int(r['timestamp'])//1000),
+            'step': int(r['step']),
+            'training_loss': float(r['value']),
+        })
+
+cols = {'timestamp': [], 'step': [], 'training_loss': []}
+for r in records:
+    for c in cols.keys():
+        cols[c].append(r[c])
+
+fig = px.line(
+    cols,
+    x='step',
+    y='training_loss',
+    markers=True,
+    template=THEME,
+    title="Training loss"
+)
+fig.update_layout(dict(
+    xaxis=dict(
+        title=dict(
+            text='Step'
+        )
+    ),
+    yaxis=dict(
+        title=dict(
+            text='Training loss'
+        )
+    )
+))
+
+fig.show()
+fig.write_html('training_loss.html', include_plotlyjs='cdn')
